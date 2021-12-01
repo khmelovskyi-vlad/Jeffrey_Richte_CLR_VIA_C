576fd72a1bb6349d53fbf996c754039791f55473
stone1234765 <51057074+stone1234765@users.noreply.github.com>
2021-12-01 15:45:05 +0200

    Add project files.

---
+++ Jeffrey_Richte_CLR_VIA_C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeffrey_Richte_CLR_VIA_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testConstuctorInh2 = new TestConstuctorInh2();
+        }
+    }
+}
---
+++ Jeffrey_Richte_CLR_VIA_C/TestConstuctors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeffrey_Richte_CLR_VIA_C
+{
+    class TestConstuctorMain
+    {
+        public TestConstuctorMain()
+        {
+            Console.WriteLine("main");
+            BadIdea();
+        }
+        protected virtual void BadIdea()
+        {
+            Console.WriteLine("main Bad Idea");
+        }
+    }
+    class TestConstuctorInh1: TestConstuctorMain
+    {
+        public TestConstuctorInh1()
+        {
+            Console.WriteLine("Inh1");
+            BadIdea();
+        }
+        protected override void BadIdea()
+        {
+            Console.WriteLine("Inh1 Bad Idea");
+        }
+    }
+    class TestConstuctorInh2: TestConstuctorInh1
+    {
+        public TestConstuctorInh2()
+        {
+            Console.WriteLine("Inh2");
+            BadIdea();
+        }
+        protected override void BadIdea()
+        {
+            Console.WriteLine("Inh2 Bad Idea");
+        }
+    }
+}
